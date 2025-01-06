af520fcc97a221efb7896835ccf69d3a31a1712e
Leandro Maximo <leandromaximo@outlook.com>
2025-01-06 16:44:14 -0300

    commit antes da criaçao dos services.

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ClientesRoutingModule } from './clientes-routing.module';
 import { ClientsFormComponent } from './clients-form/clients-form.component';
@@ -9,7 +10,10 @@ import { ClientsFormComponent } from './clients-form/clients-form.component';
   declarations: [ClientsFormComponent],
   imports: [
     CommonModule,
-    ClientesRoutingModule
+    ClientesRoutingModule,
+    FormsModule
+  ], exports: [
+    ClientsFormComponent
   ]
 })
 export class ClientesModule { }
--- src/app/clientes/clients-form/clients-form.component.ts
+++ src/app/clientes/clients-form/clients-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
 
 @Component({
   selector: 'app-clients-form',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientsFormComponent implements OnInit {
 
-  constructor() { }
+  cliente: Cliente;
+
+  constructor() {
+    this.cliente = new Cliente();
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    console.log(this.cliente);
   }
 
 }
